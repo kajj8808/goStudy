f4871c24201ad33e04233d2229511e64a2246a50
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2021-06-27 23:34:13 +0900

    2021-06-27 err , delete , add , search
    2021-06-27 err , delete , add , search

---
+++ 20210627.go
@@ -0,0 +1,59 @@
+package mydict
+
+import "errors"
+
+//Dictionary type
+type Dictionary map[string]string
+
+//erros
+var (
+	errNotFound   = errors.New("Not Found")
+	errCantUpdate = errors.New("Cant Update non-existing word")
+	errWordExists = errors.New("That word already exists")
+	errCantDelete = errors.New("Cant Delete non-existing word")
+)
+
+//Search for word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary.
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+//Update a word
+func (d Dictionary) Update(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCantDelete
+	}
+	return nil
+}
